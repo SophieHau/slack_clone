2e4639bde1a2efc0201f87babf8f1a4a9a64085f
Sophie <you@example.com>
2019-04-24 15:33:20 +0300

    adding profile functionalities

--- slack_clone/user_profile/models.py
+++ slack_clone/user_profile/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	bio = models.TextField()
-	
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	bio = models.TextField(blank=True)
+	pic = models.ImageField(upload_to='media/images/')
--- slack_clone/user_profile/views.py
+++ slack_clone/user_profile/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
 from .models import Profile
+from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
 
@@ -12,15 +14,46 @@ def login_view(request):
         if user is not None:
             login(request, user)
             profile = Profile.objects.get(user_id=user.id)
-            return redirect('user_profile:profile', profile_id=profile.id)
-        # else:
-        #     return redirect('user_profile:signup')
+            return redirect('user_profile:show_profile', profile_id=profile.id)
+        else:
+            return redirect('user_profile:signup')
 
     return render(request, 'login.html')
 
-
+@login_required(login_url='/accounts/login/')
 def show_profile(request, profile_id):
-    profile = Profile.objects.get(pk=profile_id)
+    user_profile = Profile.objects.get(pk=profile_id)
+    if request.method == 'POST':
+        if request.FILES['profile_pic']:
+          profile_pic = request.FILES['profile_pic']
+          fs = FileSystemStorage(location='media/images', base_url='/media/images')
+          filename = fs.save(profile_pic.name, profile_pic)
+          uploaded_file_url = fs.url(filename)
+          user_profile.pic = uploaded_file_url
+          user_profile.save()
+
     return render(request, 'profile.html', {
-            'profile': profile
+            'profile': user_profile,
         })
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
+
+
+def signup_view(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        bio = request.POST['bio']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        user = User.objects.create_user(email=email, first_name=first_name, last_name=last_name, username=username, password=password)
+        user.save()
+        profile = Profile(user=user, bio=bio)
+        profile.save()
+        return redirect('user_profile:show_profile', profile_id=profile.id)
+
+    return render(request, 'signup.html')
